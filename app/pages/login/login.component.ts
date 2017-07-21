75f693983710c4031f43ed8d1b8a440e3344bd03
Pradeep Vishwakarma <pradeep.vishwakarma@cennest.com>
2017-07-21 10:52:57 +0530

    fixed android action bar issue

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -4,20 +4,26 @@ import { Page } from "ui/page";
 import { hasKey, getString, setString, clear } from "application-settings";
 import * as tnsOAuthModule from 'nativescript-oauth';
 import { RouterExtensions } from "nativescript-angular/router";
+import * as application from "application";
+import { isAndroid } from "platform";
 
 @Component({
     selector: 'mkb-login',
     templateUrl: 'pages/login/login.html',
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     token: string;
 
     constructor(private router: RouterExtensions, private page: Page, private zone: NgZone) {
-        // this.page.actionBarHidden = true;
-    }
 
+    }
+    ngOnInit() {
+        if (isAndroid) {
+            this.page.actionBarHidden = true;
+        }
+    }
     tryLogin() {
         if (hasKey("accesstoken")) {
             this.router.navigate(["home"], { clearHistory: true });
